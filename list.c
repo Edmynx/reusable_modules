468a8c862c5a562d70571d5ebae740f746d743d4
Edmund Aduse Poku <edmundapoku@github.com>
2020-10-06 17:37:56 -0400

    Have made a few additions and deletions to list.c

--- list.c
+++ list.c
@@ -9,6 +9,9 @@ static void print_car(car_t *cp) {
     printf("%s\n", cp->plate);
 }
 
+/* put(): place a car at the beginning of the list
+ * returns 0 if successful; nonzero otherwise 
+ */
 int32_t lput(car_t *cp) {
     if (!cp) {
         fprintf(stderr, "Error: Value of cp is null\n");
@@ -20,9 +23,12 @@ int32_t lput(car_t *cp) {
     return 0;
 }
 
+/* get(): remove and return the first car in the list;
+ * return NULL if the list is empty
+ */
 car_t *lget() {
     if (!front) {
-        fprintf(stderr, "Error: Cannot get from an empty list\n");
+        fprintf(stderr, "Error: List is empty, returns NULL\n");
         return front;
     }
 
@@ -32,10 +38,10 @@ car_t *lget() {
     return first_element;
 }
 
+/* apply a function to every car in the list */
 void lapply(void (*fn)(car_t *cp)) {
     if (!front) {
-        fprintf(stderr, "Error: Function can not be applied to empty list\n");
-        return;
+        fprintf(stderr, "Error: Function cannot be applied to empty list\n");
     }
 
     car_t *current_car = front;
@@ -46,31 +52,32 @@ void lapply(void (*fn)(car_t *cp)) {
     }
 }
 
+/* remove(): find, remove, and return any car with
+ * the designated plate; return NULL if not present
+ */
 car_t *lremove(char *platep) {
     if (!front) {
-        fprintf(stderr, "Error: Cannot remove from an empty list\n");
+        fprintf(stderr, "Error: List is empty, returns NULL\n");
         return front;
     }
 
-    car_t *previous_car = front;
-    car_t *current_car = front;
-    car_t *result = NULL;
+    if (strcmp(front->plate, plate) == 0) {
+        lget();
+    } 
+    
+    else {
+        car_t *current_car = front->next;
+        car_t *previous_car = front;
 
-    while (current_car) {
-        if (strcmp(current_car->plate, platep) == 0) {
-            if (current_car == front) {
-                lget();
-            } else {
-                result = current_car;
-                previous_car->next = current_car->next;
-                current_car = current_car->next;
-                return result;
+        while(current_car) {
+            if (strcmp(current_car->plate, plate) == 0) {
+                previous_car->next = current_car->next
+                return current_car 
             }
+            previous_car = current_car;
+            current_car = current_car->next
         }
-
-        previous_car = current_car;
-        current_car = current_car->next;
     }
 
-    return result;
+    return NULL;
 }
