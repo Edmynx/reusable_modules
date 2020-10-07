62065b2775e0ecad1d5936e7a5a9b40c6245804a
Edmund Aduse Poku <edmundapoku@github.com>
2020-10-07 00:17:33 -0400

    Initial commit for rem-empty.c

---
+++ rem-empty.c
@@ -0,0 +1,21 @@
+/*
+ * list-test1.c -- test function for list.h
+ * remove() from an empty list
+ */
+ 
+#include <stdint.h>
+#include "list.h"
+
+int32_t main() {
+    car_t *result;
+
+    char plate_car1[] = "US-1000";
+
+    result = lremove(plate_car1);
+    if(result) {
+        printf(stderr, "Function Error: 'lremove()' should not be able to remove from an empty list\n")
+        return 1
+    }
+    
+    return 0;
+ }
