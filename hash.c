555d843683e2d5943a760fbc440288c7c96238d7
Roberto Gabriel Brito <roberto.a.gabriel.brito.22@dartmouth.edu>
2020-10-11 19:35:21 -0400

    implement most hash functions

--- hash.c
+++ hash.c
@@ -1,57 +1,166 @@
-/* 
- * hash.c -- implements a generic hash table as an indexed set of queues.
- *
- */
+#include "hash.h"
+#include "queue.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
-/* 
- * SuperFastHash() -- produces a number between 0 and the tablesize-1.
- * 
- * The following (rather complicated) code, has been taken from Paul
- * Hsieh's website under the terms of the BSD license. It's a hash
- * function used all over the place nowadays, including Google Sparse
- * Hash.
- */
-#define get16bits(d) (*((const uint16_t *) (d)))
+#define get16bits(d) (*((const uint16_t *)(d)))
 
-static uint32_t SuperFastHash (const char *data,int len,uint32_t tablesize) {
-  uint32_t hash = len, tmp;
-  int rem;
-  
-  if (len <= 0 || data == NULL)
-		return 0;
-  rem = len & 3;
-  len >>= 2;
-  /* Main loop */
-  for (;len > 0; len--) {
-    hash  += get16bits (data);
-    tmp    = (get16bits (data+2) << 11) ^ hash;
-    hash   = (hash << 16) ^ tmp;
-    data  += 2*sizeof (uint16_t);
-    hash  += hash >> 11;
-  }
-  /* Handle end cases */
-  switch (rem) {
-  case 3: hash += get16bits (data);
-    hash ^= hash << 16;
-    hash ^= data[sizeof (uint16_t)] << 18;
-    hash += hash >> 11;
-    break;
-  case 2: hash += get16bits (data);
-    hash ^= hash << 11;
+static uint32_t super_fast_hash(const char *data, int len, uint32_t tablesize) {
+    uint32_t hash = len, tmp;
+    int rem;
+
+    if (len <= 0 || data == NULL)
+        return 0;
+        
+    rem = len & 3;
+    len >>= 2;
+
+    for (; len > 0; len--) {
+        hash += get16bits(data);
+        tmp = (get16bits(data + 2) << 11) ^ hash;
+        hash = (hash << 16) ^ tmp;
+        data += 2 * sizeof(uint16_t);
+        hash += hash >> 11;
+    }
+
+    switch(rem) {
+        case 3: hash += get16bits(data);
+            hash ^= hash << 16;
+            hash ^= data[sizeof(uint16_t)] << 18;
+            hash += hash >> 11;
+            break;
+        case 2: hash += get16bits(data);
+            hash ^= hash << 11;
+            hash += hash >> 17;
+            break;
+        case 1: hash += *data;
+            hash ^= hash << 10;
+            hash += hash >> 11;
+    }
+
+    hash ^= hash << 3;
+    hash += hash >> 5;
+    hash ^= hash << 4;
     hash += hash >> 17;
-    break;
-  case 1: hash += *data;
-    hash ^= hash << 10;
-    hash += hash >> 1;
-  }
-  /* Force "avalanching" of final 127 bits */
-  hash ^= hash << 3;
-  hash += hash >> 5;
-  hash ^= hash << 4;
-  hash += hash >> 17;
-  hash ^= hash << 25;
-  hash += hash >> 6;
-  return hash % tablesize;
+    hash ^= hash << 25;
+    hash += hash >> 6;
+    return hash % tablesize;
 }
 
+struct hashtable {
+    uint32_t size;
+    uint32_t length;
+    queue_t **table;
+};
+
+hashtable_t *hopen(uint32_t hsize) {
+    if (hsize < 1) {
+        fprintf(stderr, "Error: Invalid size provided");
+        return NULL;
+    }
+
+    struct hashtable* htp = malloc(sizeof(struct hashtable));
+
+    if (!htp) {
+        fprintf(stderr, "Error: Failed to allocate memory");
+        return NULL;
+    }
+
+    queue_t **table = malloc(sizeof(queue_t *) * hsize);
+    
+    if (!table) {
+        fprintf(stderr, "Error: Failed to allocate memory");
+        return NULL;
+    }
+
+    htp->size = hsize;
+    htp->length = 0;
+    htp->table = table;
+    
+    return (hashtable_t *) htp;
+}
+
+void hclose(hashtable_t *htp) {
+    if (!htp) {
+        fprintf(stderr, "Error: htp is null");
+        return;
+    }
+
+    struct hashtable *hash_ptr = htp;
+
+    for (int i = 0; i < hash_ptr->length; ++i) {
+        qclose(hash_ptr->table[i]);
+    }
+
+    free(hash_ptr->table);
+    free(hash_ptr);
+}
+
+int32_t hput(hashtable_t *htp, void *ep, const char *key, int keylen);
+
+void happly(hashtable_t *htp, void (*fn)(void* ep)) {
+    if (!htp) {
+        fprintf(stderr, "Error: htp is null");
+        return;
+    }
+
+    struct hashtable *hash_ptr = htp;
+
+    if (hash_ptr->length == 0) {
+        fprintf(stderr, "Error: hashtable is empty");
+        return;
+    }
+
+    for (int i = 0; i < hash_ptr->length; ++i) {
+        qapply(hash_ptr->table[i], fn);
+    }
+}
+
+void *hsearch(hashtable_t *htp, bool (*searchfn)(void* elementp, const void* searchkeyp), const char *key, int32_t keylen) {
+    if (!htp) {
+        fprintf(stderr, "Error: htp is null");
+        return NULL;
+    }
+
+    struct hashtable *hash_ptr = htp;
+
+    if (hash_ptr->length == 0) {
+        fprintf(stderr, "Error: hashtable is empty");
+        return NULL;
+    }
+
+    uint32_t index = super_fast_hash(key, keylen, hash_ptr->length);
+    return qsearch(hash_ptr->table[index], searchfn, key);
+}
+
+void *hremove(hashtable_t *htp, bool (*searchfn)(void* elementp, const void* searchkeyp), const char *key, int32_t keylen) {
+    if (!htp) {
+        fprintf(stderr, "Error: htp is null");
+        return NULL;
+    }
+
+    struct hashtable *hash_ptr = htp;
+
+    if (hash_ptr->length == 0) {
+        fprintf(stderr, "Error: hashtable is empty");
+        return NULL;
+    }
+
+    uint32_t index = super_fast_hash(key, keylen, hash_ptr->length);
+    return qremove(hash_ptr->table[index], searchfn, key);
+}
+
+int main(void) {
+    struct hashtable *htp = hopen(5);
+    queue_t *queue = qopen();
+    int elem = 1;
+
+    qput(queue, (void *) &elem);
+    htp->table[0] = queue;
+    htp->length++;
+
+    printf("%d\n", *(int *)qget(htp->table[0]));
+    hclose(htp);
+}
